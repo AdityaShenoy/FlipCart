d4ad73743b8a4641887dffee8a2b69fb17b94949
Aditya Shenoy <adityashenoy49@gmail.com>
2024-08-11 22:31:41 +0530

    fix: add inheritance from django model class

--- items/models.py
+++ items/models.py
@@ -3,7 +3,7 @@ import django.db.models as ddm
 import users.models as um
 
 
-class Item:
+class Item(ddm.Model):
     name = ddm.CharField(max_length=50)
     description = ddm.CharField(max_length=500)
     seller = ddm.ForeignKey(um.User, on_delete=ddm.CASCADE)

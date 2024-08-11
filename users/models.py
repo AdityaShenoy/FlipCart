6d3c84b7de0e00ae79ceb387886883913f07bd3b
Aditya Shenoy <adityashenoy49@gmail.com>
2024-08-11 22:14:39 +0530

    style: remove unnecessary comments

--- users/models.py
+++ users/models.py
@@ -2,7 +2,6 @@ import django.contrib.auth.models as dcam
 import django.db.models as ddm
 
 
-# Create your models here.
 class User(dcam.AbstractUser):
     username = ddm.CharField(max_length=50, unique=True)
     email = ddm.EmailField(max_length=50)

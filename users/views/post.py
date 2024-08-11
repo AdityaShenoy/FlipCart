992afd232465c53fc3c09d4569e5663ca676c732
Aditya Shenoy <adityashenoy49@gmail.com>
2024-08-11 22:33:17 +0530

    fix: populate name and email fields in the database

--- users/views/post.py
+++ users/views/post.py
@@ -7,5 +7,14 @@ import users.models as um
 def post(request: dhreq.HttpRequest):
     username = request.POST.get("username", "")
     password = request.POST.get("password", "")
-    um.User.objects.create_user(username=username, password=password)
+    email = request.POST.get("email", "")
+    first_name = request.POST.get("first_name", "")
+    last_name = request.POST.get("last_name", "")
+    um.User.objects.create_user(
+        username=username,
+        password=password,
+        email=email,
+        first_name=first_name,
+        last_name=last_name,
+    )
     return fcru.USER_CREATED

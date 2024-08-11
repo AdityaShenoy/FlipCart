af5633ffb5ffb46c564e5c4356bd173e23b1a0ab
Aditya Shenoy <adityashenoy49@gmail.com>
2024-08-11 22:15:14 +0530

    feat: add item model

---
+++ items/models.py
@@ -0,0 +1,11 @@
+import django.db.models as ddm
+
+import users.models as um
+
+
+class Item:
+    name = ddm.CharField(max_length=50)
+    description = ddm.CharField(max_length=500)
+    seller = ddm.ForeignKey(um.User, on_delete=ddm.CASCADE)
+    mrp = ddm.IntegerField()
+    price = ddm.IntegerField()

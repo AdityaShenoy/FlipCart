c41c17e37c6be22e180a053017a0b6a60dc5451e
Aditya Shenoy <adityashenoy49@gmail.com>
2024-08-28 08:40:03 +0530

    feat: add a helper function to create response with data

--- flip_cart/constants/responses/create_response.py
+++ flip_cart/constants/responses/create_response.py
@@ -3,6 +3,20 @@ import rest_framework.response as rr  # type: ignore
 import typing as t
 
 
-def create_response(message: str, status: int, data: dict[str, t.Any] = {}):
+class CustomResponse(rr.Response):
+    data: dict[str, t.Any] = dict()
 
-    return rr.Response(data={"message": message, **data}, status=status)
+
+def create_response(message: str, status: int):
+
+    return rr.Response(data={"message": message}, status=status)
+
+
+def create_data_response(
+    template_response: CustomResponse,
+    data: dict[str, t.Any],
+):
+    return CustomResponse(
+        data={**template_response.data, **data},
+        status=template_response.status_code,
+    )

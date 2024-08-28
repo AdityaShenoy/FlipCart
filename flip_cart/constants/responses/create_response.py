e7059c624e64a1b7eefa105321d57c506bdcb58d
Aditya Shenoy <adityashenoy49@gmail.com>
2024-08-28 08:54:18 +0530

    fix: remove unused custom response class added for type hinting

--- flip_cart/constants/responses/create_response.py
+++ flip_cart/constants/responses/create_response.py
@@ -3,20 +3,16 @@ import rest_framework.response as rr  # type: ignore
 import typing as t
 
 
-class CustomResponse(rr.Response):
-    data: dict[str, t.Any] = dict()
-
-
 def create_response(message: str, status: int):
 
     return rr.Response(data={"message": message}, status=status)
 
 
 def create_data_response(
-    template_response: CustomResponse,
+    template_response: rr.Response,
     data: dict[str, t.Any],
 ):
-    return CustomResponse(
-        data={**template_response.data, **data},
+    return rr.Response(
+        data={**template_response.data, **data},  # type: ignore
         status=template_response.status_code,
     )
